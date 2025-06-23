74859aaa8004ebef718fa61bc1c22366807a68a8
Muhammad Awais <awaisshaikh94@gmail.com>
2025-06-23 18:21:51 +0500

    added bootstrap

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,11 +5,13 @@ import AiForecastNaive from './pages/ai-forecast-naive'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AiForecastNaive />} />
-      <Route path="/ai-forecast-naive" element={<AiForecastNaive />} />
-      <Route path="/ai-forecast-ml" element={<AiForecastML />} />
-    </Routes>
+    <div className="container mt-5">
+      <Routes>
+        <Route path="/" element={<AiForecastNaive />} />
+        <Route path="/ai-forecast-naive" element={<AiForecastNaive />} />
+        <Route path="/ai-forecast-ml" element={<AiForecastML />} />
+      </Routes>
+    </div>
   )
 }
 
